0d79415e2eeafbd7e78078917e279e4fdff17a77
Bogdan Devichev <20227856+BogdanDevichev@users.noreply.github.com>
2019-08-03 18:00:25 +0300

    Pickaxe adding. Step 1. Prefabs added.

---
+++ Assets/Scripts/ItemScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("TRIGIRRED!!!! (2D)");
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("TRIGIRRED!!!! (Player)");
+            Destroy(gameObject);
+            //collision.gameObject.getItem();
+        }
+    }
+}

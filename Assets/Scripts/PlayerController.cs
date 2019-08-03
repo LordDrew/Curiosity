a1aa7fb583af1927efd4eb9b7e1252b740b76dbb
LordDrew <LordDrew@users.noreply.github.com>
2019-08-03 17:23:13 +0300

    Jump only from the ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public LayerMask groundLayer;
     public float speed = 10f;
+    public float jumpHeight = 1f;
+
+    float groundRaycastDistance = 0.6f;
+
     new Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +21,26 @@ public class PlayerController : MonoBehaviour
     {
         var horizontalMovement = Input.GetAxis("Horizontal") * speed;
         rigidbody.AddForce(horizontalMovement * Vector2.right);
-        if (Input.GetButton("Jump"))
-            rigidbody.AddForce(Vector2.up * 30);
+        if (Input.GetButton("Jump") && IsGrounded())
+            rigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+    }
+
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, groundRaycastDistance, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Debug.DrawRay(transform.position, Vector2.down*groundRaycastDistance, Color.green);
     }
 }

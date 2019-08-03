785fc0c4a415c32a09726dfae74e4089a6b9e15f
LordDrew <LordDrew@users.noreply.github.com>
2019-08-03 15:41:44 +0300

    Add basic prefabs: player and tile

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    new Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        var horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        rigidbody.AddForce(horizontalMovement * Vector2.right);
+        if (Input.GetButton("Jump"))
+            rigidbody.AddForce(Vector2.up * 30);
+    }
+}

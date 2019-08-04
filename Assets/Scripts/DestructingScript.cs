80ac19080b131e66ac7f80f81cd9bed1513e77ee
Bogdan Devichev <20227856+BogdanDevichev@users.noreply.github.com>
2019-08-04 15:18:50 +0300

    Destructing is added.

---
+++ Assets/Scripts/DestructingScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructingScript : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Coll Destructing");
+        if (collision.gameObject.GetComponent<PlayerController>().spriteRenderer.color == gameObject.GetComponent<DestructingScript>().spriteRenderer.color)
+        {
+            Debug.Log("Correct Type :)");
+        }
+        else
+        {
+            Debug.Log("Boom!!!Game Over=(");
+            Destroy(collision.gameObject);
+        }
+    }
+}

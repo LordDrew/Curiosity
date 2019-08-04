eba036909c4fe3e4829744279eac6029407fe762
LordDrew <LordDrew@users.noreply.github.com>
2019-08-04 20:10:43 +0300

    Make blue tiles destructable

---
+++ Assets/Scripts/TileDestroyer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileDestroyer : MonoBehaviour
+{
+    public UnityEngine.Tilemaps.Tilemap tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 hitPosition = Vector3.zero;
+        if (tilemap != null)
+        {
+            foreach (ContactPoint2D hit in collision.contacts)
+            {
+                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+            }
+        }
+    }
+}

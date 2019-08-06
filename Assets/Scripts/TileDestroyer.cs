2f87323c8e44c18476b5d8ba2bc14bb5b28ec6d9
LordDrew <LordDrew@users.noreply.github.com>
2019-08-06 23:52:16 +0300

    Do not collide with blue tiles when breaking them

--- Assets/Scripts/TileDestroyer.cs
+++ Assets/Scripts/TileDestroyer.cs
@@ -24,4 +24,17 @@ public class TileDestroyer : MonoBehaviour
             }
         }
     }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Vector3 hitPosition = Vector3.zero;
+        if (tilemap != null)
+        {
+            foreach (ContactPoint2D hit in collision.contacts)
+            {
+                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+            }
+        }
+    }
 }
